22df5f006409c215a6856c954eb3d32d19ec8fdc
liuning <liuning@espressif.com>
2022-12-25 18:36:45 +0800

    esp_coex: init (b3e8cbe2)

---
+++ fix_printf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+for dir in esp32 esp32s2 esp32c3 esp32s3 esp32c2 esp32c6; do
+    if [ $dir = esp32 ]; then
+        TOOLCHAIN="xtensa-esp32-elf"
+    elif [ $dir = esp32s2 ]; then
+        TOOLCHAIN="xtensa-esp32s2-elf"
+    elif [ $dir = esp32c3 -o $dir = esp32c2 -o $dir = esp32c6 ]; then
+        TOOLCHAIN="riscv32-esp-elf"
+    elif [ $dir = esp32s3 ]; then
+        TOOLCHAIN="xtensa-esp32s3-elf"
+    else
+        echo "$dir does not exist"
+    fi
+    if [ -d "$dir" ]; then
+        chmod -x $dir/*;
+        cd $dir
+
+        git status libcoexist.a | grep "modified" >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $dir/libcoexist.a fixed
+            $TOOLCHAIN-objcopy --redefine-sym ets_printf=coexist_printf libcoexist.a
+            $TOOLCHAIN-objcopy --redefine-sym printf=coexist_printf libcoexist.a
+        fi
+
+        cd ..
+    else
+        echo "$dir does not exist"
+    fi
+done;
