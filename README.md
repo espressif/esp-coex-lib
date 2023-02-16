cc8eae05caf9b67ddbde40b057e32748c197b95b
liuning <liuning@espressif.com>
2023-02-16 11:21:44 +0800

    esp_coex: separate coex lib from wifi lib (9747c21e)

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 ESP32 Coexist Libraries
 ====================
 
-This repository contains binary libraries supporting the ESP32 series coexistence subsystems. It is used as a submodule within `Espressif IoT Development Framework`_ (ESP-IDF).
+This repository contains binary libraries supporting the ESP32 series Wi-Fi/Bluetooth/802.15.4 coexistence subsystems. It is used as a submodule within [Espressif IoT Development Framework](https://github.com/espressif/esp-idf).
 
-Files in this repository are Copyright (C) 2015-2016 Espressif Systems.
+Files in this repository are Copyright (C) 2015-2023 Espressif Systems.
 
 These binary libraries are provided under the same license as the parent esp-idf project - the Apache License 2.0 as provided in the file LICENSE. (The license text refers to this as "Object" form.)
-
-.. _Espressif IoT Development Framework: https://github.com/espressif/esp-idf
--- tools/ci/build_app.sh
+++ tools/ci/build_app.sh
@@ -2,19 +2,18 @@
 # This file is sourced in to the CI environment
 # in .gitlab-ci.yml file
 
-set -x
+set -euo pipefail
 
 die() {
     echo "${1:-"Unknown Error"}" 1>&2
-    exit 1
 }
 
 [ -z ${IDF_PATH} ] && die "IDF_PATH is not set."
 
 # Set Env
 cd ${CI_PROJECT_DIR}/esp-idf
-./install.sh || { echo "Install Failure"; exit 1; }
-. ./export.sh || { echo "Export Failure"; exit 1; }
+./install.sh || { echo "Install Failure"; }
+. ./export.sh || { echo "Export Failure"; }
 
 # Set Env
 cd ${CI_PROJECT_DIR}/SSC
@@ -35,10 +34,10 @@ rm -rf ${board}-${CI_PIPELINE_ID}.debug
 mkdir -p ${board}-${CI_PIPELINE_ID}.debug/
 
 echo Creating ${board}-${VERSION} app binaries debug directory...
-cp ${CI_PROJECT_DIR}/SSC/build/ssc.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
-cp ${CI_PROJECT_DIR}/SSC/build/ssc.elf $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
-cp ${CI_PROJECT_DIR}/SSC/build/bootloader/bootloader.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
-cp ${CI_PROJECT_DIR}/SSC/build/partition_table/partition-table.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; exit 1; }
+cp ${CI_PROJECT_DIR}/SSC/build/ssc.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; }
+cp ${CI_PROJECT_DIR}/SSC/build/ssc.elf $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; }
+cp ${CI_PROJECT_DIR}/SSC/build/bootloader/bootloader.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; }
+cp ${CI_PROJECT_DIR}/SSC/build/partition_table/partition-table.bin $board-${CI_PIPELINE_ID}.debug/ || { echo "Copy file Status: Failure"; }
 
 # Creating tar file (debug)
-tar -zcvf $board-${CI_PIPELINE_ID}.debug.tar.gz $board-${CI_PIPELINE_ID}.debug || { echo "Create tar Status: Failure"; exit 1; }
+tar -zcvf $board-${CI_PIPELINE_ID}.debug.tar.gz $board-${CI_PIPELINE_ID}.debug || { echo "Create tar Status: Failure"; }
